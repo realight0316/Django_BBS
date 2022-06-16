351032e7de176f5b91f93965f8a2f340f0fabc58
realight0316 <0316kjh@naver.com>
2022-06-17 02:51:06 +0900

    Feat: Add borad url 지정

--- webpage_project/webpage_project/urls.py
+++ webpage_project/webpage_project/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from board.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('board/', board, name='board'),
 ]
