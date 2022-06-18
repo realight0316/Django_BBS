082fd96e9c4d5d7214014562ffdfee5c5f1c97ac
realight0316 <0316kjh@naver.com>
2022-06-19 01:11:38 +0900

    feat: Add 게시판, 글 작성, 공고 내용 html 연결

--- webpage_project/board/views.py
+++ webpage_project/board/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
+from board.forms import *
 
 # Create your views here.
 
+def write(request):
+    if request.method == 'POST':
+        form = Form(request.POST)
+        form.company_id = request.user
+        if form.is_valid():
+            form.save()
+    else:
+        form = Form()
+    return render(request, 'write.html', {'form':form})
+
 def board(request):
-    return render(request, 'board.html')
+    articleList = Article.objects.all()
+    return render(request, 'board.html', {'articleList': articleList})
+
+def view(request, num="1"):
+    article = Article.objects.get(post_id=num)
+
+    return render(request, 'list.html', {'article':article})
