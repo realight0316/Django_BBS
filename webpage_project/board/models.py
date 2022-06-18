8f4c2785974c8f002f8731c4367bf3c39958ef86
realight0316 <0316kjh@naver.com>
2022-06-19 01:13:31 +0900

    Feat: Add 회사정보 db 구성

--- webpage_project/board/models.py
+++ webpage_project/board/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
@@ -9,5 +10,12 @@ class Article(models.Model):
     awards = models.IntegerField()
     contents = models.TextField()
     skills = models.CharField(max_length=50)
-    write_date = models.DateTimeField(auto_now_add=True)
+    write_date = models.DateTimeField(default=timezone.now)
+    # write_date = models.DateTimeField(auto_now_add=True)
     # 공고문id, 회사id, 포지션명, 취업보상금, 채용내용, 사용기술, 작성일자
+
+class Company(models.Model):
+    company_id = models.AutoField(primary_key=True)
+    company_name = models.CharField(max_length=20)
+    nation = models.CharField(max_length=20)
+    region = models.CharField(max_length=20, null=True)
