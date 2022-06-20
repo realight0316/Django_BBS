f1ac6395af9084805694320961b3dd95fe364882
realight0316 <0316kjh@naver.com>
2022-06-20 23:14:43 +0900

    Feat: Add 회사정보 모델 추가 및 수정

--- webpage_project/board/models.py
+++ webpage_project/board/models.py
@@ -1,21 +1,35 @@
+import re
+from tabnanny import verbose
 from django.db import models
 from django.utils import timezone
 
 # Create your models here.
 
 class Article(models.Model):
-    post_id = models.AutoField(primary_key=True)
-    company_id = models.IntegerField()
-    position_name = models.CharField(max_length=50)
-    awards = models.IntegerField()
-    contents = models.TextField()
-    skills = models.CharField(max_length=50)
-    write_date = models.DateTimeField(default=timezone.now)
+    post_id = models.AutoField(verbose_name='공고id ', primary_key=True)
+    company_id = models.ForeignKey('users.Users', verbose_name='회사id', on_delete=models.CASCADE)
+    position_name = models.CharField(verbose_name='포지션명', max_length=50)
+    awards = models.IntegerField(verbose_name='취업보상금')
+    contents = models.TextField(verbose_name='채용내용')
+    skills = models.CharField(verbose_name='사용기술', max_length=50)
+    write_date = models.DateTimeField(verbose_name='작성일자', default=timezone.now)
     # write_date = models.DateTimeField(auto_now_add=True)
-    # 공고문id, 회사id, 포지션명, 취업보상금, 채용내용, 사용기술, 작성일자
+
+    class Meta:
+        db_table = "articles"       # db테이블명 정의
+        verbose_name = "채용공고 정보"
 
 class Company(models.Model):
-    company_id = models.AutoField(primary_key=True)
-    company_name = models.CharField(max_length=20)
-    nation = models.CharField(max_length=20)
-    region = models.CharField(max_length=20, null=True)
+    company_id = models.CharField(verbose_name='회사id', max_length=30, primary_key=True)
+    company_pw = models.CharField(verbose_name='회사pw', max_length=50)
+    company_name = models.CharField(verbose_name='회사명', max_length=20)
+    nation = models.CharField(verbose_name='국가', max_length=20)
+    region = models.CharField(verbose_name='지역', max_length=20, null=True)
+
+    def __str__(self):
+        return self.company_name        # 관리자 화면에서 보이는 정보표기
+
+    class Meta:
+        db_table = "company_info"       # db테이블명 정의
+        verbose_name = "회사 정보"
+
