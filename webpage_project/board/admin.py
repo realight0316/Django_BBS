05df68053b37d393f2923c5fcd7fbdb60157930e
realight0316 <0316kjh@naver.com>
2022-06-21 23:14:27 +0900

    Feat: Add admin 페이지 company 모델 등록

--- webpage_project/board/admin.py
+++ webpage_project/board/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Company
 
 # Register your models here.
+
+class companyAdmin(admin.ModelAdmin):
+    list_display = ('company_id', 'company_name', 'nation')
+
+admin.site.register(Company, companyAdmin)
