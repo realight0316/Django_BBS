1ac2408b0111cdf00986dbc712cf781056f297c4
realight0316 <0316kjh@naver.com>
2022-06-22 23:42:11 +0900

    Feat: Add signup 뷰함수 생성

--- webpage_project/board/views.py
+++ webpage_project/board/views.py
@@ -1,11 +1,36 @@
 from django.shortcuts import render
 from board.forms import *
+from models import Company
+from django.contrib.auth.hashers import make_password, check_password
 
 # Create your views here.
 
 def signup(request):
     if request.method == 'GET':
         return render(request, 'signup.html')
+    elif request.method == 'POST':
+        company_name = request.POST.get('company_name', None)
+        company_id = request.POST.get('company_id', None)
+        company_pw = request.POST.get('company_pw', None)
+        re_company_pw = request.POST.get('re_company_pw', None)
+        nation = request.POST.get('nation', None)
+        region = request.POST.get('region', None)
+        errorMsg = {}
+
+        if not (company_id and company_pw and re_company_pw and nation):
+            errorMsg['error'] = "모든 값을 입력해야 합니다."
+        elif company_pw != re_company_pw:
+            errorMsg['error'] = '비밀번호가 다릅니다.'
+        else:
+            company_info = Company(
+                company_id = company_id,
+                company_pw = make_password(company_pw),
+                company_name = company_name,
+                nation = nation,
+                region = region
+            )
+            company_info.save()
+        return render(request, 'signup.html', errorMsg)
 
 def write(request):
     if request.method == 'POST':
