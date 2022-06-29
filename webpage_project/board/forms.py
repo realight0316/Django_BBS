74615a106c8b3d87f896f5f1abc497694bb8fdd1
realight0316 <0316kjh@naver.com>
2022-06-29 23:11:43 +0900

    Feat: Add 게시판 글 입력 폼 클래스 생성

--- webpage_project/board/forms.py
+++ webpage_project/board/forms.py
@@ -1,7 +1,14 @@
+from distutils.log import error
 from django.forms import ModelForm
 from board.models import *
+from django import forms
 
 class Form(ModelForm):
     class Meta:
         model = Article
         fields = ['position_name', 'awards',  'contents', 'skills', 'write_date']
+
+class articleForm(forms.Form):
+    position_name = forms.CharField(error_messages={'required': "포지션명을 입력하시오"}, lebel = '포지션명', max_length=50)
+    contents = forms.CharField(error_messages={'required':'내용을 입력하시오'}, label="채용내용", widget= forms.Textarea)
+    awards = forms.IntegerField(error_messages={'required': "보상금을 입력하시오"}, label="채용보상금")
